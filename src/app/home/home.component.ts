3b94b7b6b2688cf7ccdbad1a013c9e1457765749
Vincent D'amour <funnyvinc3@gmail.com>
2016-12-18 12:50:38 -0500

    Home page done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() {
-    // Do stuff
-  }
-
-  ngOnInit() {
-    console.log('Hello Home');
-  }
-
+export class HomeComponent {
 }

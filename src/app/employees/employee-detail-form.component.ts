c034437ec2e9f175542d1d24822000022af3523d
Vincent D'amour <funnyvinc3@gmail.com>
2016-12-21 22:33:26 -0500

    set file upload

--- src/app/employees/employee-detail-form.component.ts
+++ src/app/employees/employee-detail-form.component.ts
@@ -19,4 +19,50 @@ export class EmployeeDetailFormComponent {
   onDelete() {
     this.delete.emit();
   }
+
+  uploadFile(event) {
+    const file = event.srcElement.files[0];
+
+    if(file && file.size < 1024 * 1024 * 10) {
+      const reader = new FileReader();
+
+      reader.onload = event => {
+        const url = event.target['result'];
+
+        const binary = atob(url.split(',')[1]);
+        const array = [];
+        for(let i = 0; i < binary.length; i++) {
+          array.push(binary.charCodeAt(i));
+        }
+
+        // send array to server to save directly binary data
+
+        // C# code
+        // Byte[] bytes = get bytes from db;
+        // HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+        // System.IO.MemoryStream stream = new MemoryStream(bytes);
+        // result.Content = new StreamContent(stream);
+        // return (result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  uploadFileTwo(event) {
+    const file = event.srcElement.files[0];
+
+    if(file && file.size < 1024 * 1024 * 10) {
+      const formData = new FormData();
+      formData.append('uploads[]', file, file.name);
+
+      // Do a post request with formData as the data. Be sure to set processData and contentType to false
+      // { url: 'file', data: formData, processData: false, contentType: false }
+
+      // C# side
+      // Read bytes from DB
+      // Copy file (from bytes) to a download folder with the file name + unique hash
+      // Send route to file
+    }
+  }
 }
